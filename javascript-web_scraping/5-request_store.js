498dde666c00a35a0e87b45c3a6726422023f2ae
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-19 13:07:36 +0400

    Semistandard validation errors

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,9 +6,14 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) throw error;
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (error) throw error;
-  });
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
