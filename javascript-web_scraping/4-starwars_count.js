d16e0c2bfd19a6346dbd5fbdd83a3843680feb4c
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-19 12:54:29 +0400

    Fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,11 +5,11 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (error) throw error
+  if (error) throw error;
   let count = 0;
   const films = JSON.parse(body).results;
   films.forEach(film => {
-   film.characters.forEach(character => {
+  film.characters.forEach(character => {
     if (character.includes('/18/')) {
       count++;
      }
